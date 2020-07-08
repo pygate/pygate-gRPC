b0d466726ea145fef75b885448927ad3ed3617db
Leonard Ge <wge@confluent.io>
2020-07-08 19:08:39 +0100

    Added some API for ffs.

--- pygate_grpc/client.py
+++ pygate_grpc/client.py
@@ -1,14 +1,20 @@
 import health
 import faults
 import deals
+import ffs
 
 class PowerGateClient(object):
     def __init__(self, hostName):
         self.health = health.HealthClient(hostName)
         self.faults = faults.FaultsClient(hostName)
         self.deals = deals.DealsClient(hostName)
+        self.ffs = ffs.FfsClient(hostName)
 
 
 if __name__ == "__main__":
     c = PowerGateClient("127.0.0.1:5002")
-    print(c.faults.get())
+    # print()
+    # print("creating!", c.ffs.create())
+    # print("listing!", c.ffs.listApi())
+    # print("addrs list!", c.ffs.addrsList("e795b35b-0c52-4b0d-9791-06334e0c52f0"))
+    print("info!", c.ffs.info("QmR74AUggas4mmed9zBAH3NyeeBWzgtDfxidZuxMrLBJw7", "e795b35b-0c52-4b0d-9791-06334e0c52f0"))
--- pygate_grpc/ffs.py
+++ pygate_grpc/ffs.py
@@ -1,13 +1,49 @@
 import grpc
 
-import proto.faults_rpc_pb2 as faults_rpc_pb2 
-import proto.faults_rpc_pb2_grpc as faults_rpc_pb2_grpc
+import proto.ffs_rpc_pb2 as ffs_rpc_pb2
+import proto.ffs_rpc_pb2_grpc as ffs_rpc_pb2_grpc
 
-class FaultsClient(object):
+TOKEN_KEY = 'x-ffs-token'
+
+class FfsClient(object):
     def __init__(self, hostName):
+        self.hostName = hostName
         channel = grpc.insecure_channel(hostName)
-        self.client = faults_rpc_pb2_grpc.RPCServiceStub(channel)
+        self.client = ffs_rpc_pb2_grpc.RPCServiceStub(channel)
 
-    def get(self):
-        req = faults_rpc_pb2.GetRequest()
-        return self.client.Get(req)
+    def _getMetaData(self, token):
+        return ((TOKEN_KEY, token),)
+
+    def create(self):
+        req = ffs_rpc_pb2.CreateRequest()
+        return self.client.Create(req)
+
+    def listApi(self):
+        req = ffs_rpc_pb2.ListAPIRequest()
+        return self.client.ListAPI(req)
+
+
+    # The metadata is set in here https://github.com/textileio/js-powergate-client/blob/9d1ad04a7e1f2a6e18cc5627751f9cbddaf6fe05/src/util/grpc-helpers.ts#L7
+    # Note that you can't have capital letter in meta data field, see here: https://stackoverflow.com/questions/45071567/how-to-send-custom-header-metadata-with-python-grpc
+    def addrsList(self, token):
+        req = ffs_rpc_pb2.AddrsRequest()
+        return self.client.Addrs(req, metadata=self._getMetaData(token))
+
+    def iD(self, token):
+        req = ffs_rpc_pb2.IDRequest()
+        return self.client.ID(req, metadata=self._getMetaData(token))
+
+    def addToHot(self, chunks):
+        # TODO: need to figure out how to send file chunk to server
+        pass
+
+    def logs(self, cid, token):
+        req = ffs_rpc_pb2.WatchLogsRequest()
+        req.setCid(cid)
+        return self.client.WatchLogs(req, metadata=self._getMetaData(token))
+
+    def info(self, cid, token):
+        req = ffs_rpc_pb2.WatchLogsRequest(cid=cid)
+        return self.client.Info(req, metadata=self._getMetaData(token))
+
+
