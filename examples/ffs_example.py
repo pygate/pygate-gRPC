1761601161874966e0fbe68463b939fafc4529e3
Peter Van Garderen <peter@artefactual.com>
2020-07-09 10:22:52 -0700

    fix channel variable

--- examples/ffs_example.py
+++ examples/ffs_example.py
@@ -2,7 +2,7 @@ import grpc
 from pygate_grpc.client import PowerGateClient
 
 
-channel = grpc.insecure_channel("127.0.0.1:5002")
-client = PowerGateClient(channel)
+client = PowerGateClient("127.0.0.1:5002")
+
 newFfs = client.ffs.create()
 print(newFfs)
