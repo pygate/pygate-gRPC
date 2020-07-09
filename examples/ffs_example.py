67bfe8e2f4e6a069c2c3573f162cb68dc659945b
Peter Van Garderen <peter@artefactual.com>
2020-07-09 09:56:45 -0700

    WIP: ffs example script

---
+++ examples/ffs_example.py
@@ -0,0 +1,8 @@
+from grpcio import grpc
+from pygate_grpc.client import PowerGateClient
+
+
+channel = grpc.insecure_channel("127.0.0.1:5002")
+client = PowerGateClient(channel)
+newFfs = client.ffs.create()
+print(newFfs)
