b29bb156dd246ee31b73cd9d95a677d69273cff0
Peter Van Garderen <peter@artefactual.com>
2020-07-12 10:14:38 -0700

    add print output to healthcheck example

--- examples/simple_healthcheck.py
+++ examples/simple_healthcheck.py
@@ -2,4 +2,6 @@ from pygate_grpc.client import PowerGateClient
 
 client = PowerGateClient("127.0.0.1:5002")
 
+print("Checking node health...")
 healthcheck = client.health.check()
+print(healthcheck)
