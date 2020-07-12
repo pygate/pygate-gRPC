1b3b963adeaaa27ed6f65938f54454fe6a04f719
Peter Van Garderen <peter@artefactual.com>
2020-07-12 11:01:51 -0700

    format print statements

--- examples/ffs_push_pull_file.py
+++ examples/ffs_push_pull_file.py
@@ -11,15 +11,15 @@ if __name__ == "__main__":
     print(ffs)
     # Create an iterator of the given file using the helper function
     iter = c.ffs.get_file_bytes("README.md")
-    print("grabbing pygate-grpc 'README.md' file")
-    print("adding file to IPFS (hot storage)")
+    print("Grabbing pygate-grpc 'README.md' file...")
+    print("Adding file to IPFS (hot storage)...")
     # Convert the iterator into request and then add to hot set
     res = c.ffs.add_to_hot(c.ffs.bytes_to_chunks(iter), ffs.token)
     print(res)
-    print("pushing file to FFS")
+    print("Pushing file to FFS...")
     # Push the given file
     c.ffs.push(res.cid, ffs.token)
-    print("retrieving file from FFS:")
     # Get the file back
     file = c.ffs.get(res.cid, ffs.token)
+    print("Retrieved file from FFS:")
     print(next(file))
