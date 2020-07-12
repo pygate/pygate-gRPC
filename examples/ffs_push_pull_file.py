73e3455fcbe1aa0727ad770c35958c3c4f52890c
Peter Van Garderen <peter@artefactual.com>
2020-07-12 10:49:37 -0700

    rename. add print output.

--- examples/ffs_push_pull_file.py
+++ examples/ffs_push_pull_file.py
@@ -7,12 +7,19 @@ if __name__ == "__main__":
     c = PowerGateClient(hostName)
     # Create FFS
     ffs = c.ffs.create()
+    print("FFS created:")
+    print(ffs)
     # Create an iterator of the given file using the helper function
     iter = c.ffs.get_file_bytes("README.md")
+    print("grabbing pygate-grpc 'README.md' file")
+    print("adding file to IPFS (hot storage)")
     # Convert the iterator into request and then add to hot set
     res = c.ffs.add_to_hot(c.ffs.bytes_to_chunks(iter), ffs.token)
+    print(res)
+    print("pushing file to FFS")
     # Push the given file
     c.ffs.push(res.cid, ffs.token)
+    print("retrieving file from FFS:")
     # Get the file back
     file = c.ffs.get(res.cid, ffs.token)
     print(next(file))
