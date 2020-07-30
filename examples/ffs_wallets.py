a1eb136e869946559dffdd619ccf0aa63969a563
EzeChinonso <48421032+EzeChinonso@users.noreply.github.com>
2020-07-30 11:29:58 +0100

    Update ffs_wallets.py

--- examples/ffs_wallets.py
+++ examples/ffs_wallets.py
@@ -1,4 +1,6 @@
 from pygate_grpc.client import PowerGateClient
+from google.protobuf.json_format import MessageToJson
+import json
 
 client = PowerGateClient("127.0.0.1:5002")
 
@@ -11,6 +13,12 @@ wallet = client.ffs.addrs_list(newFfs.token)
 print("Wallet address:")
 print(wallet)
 
+jsonObj = MessageToJson(wallet)
+#print(j)
+info = json.loads(jsonObj)
+print(info['addrs'][0]['addr'])
 
 print("wallet type:")
 print(type(wallet))
+
+
