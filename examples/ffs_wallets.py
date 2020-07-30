545559748798e4981ad7c7b217e9c59563fcc487
Peter Van Garderen <peter@artefactual.com>
2020-07-30 13:00:59 -0700

    WIP: list wallets and balance

--- examples/ffs_wallets.py
+++ examples/ffs_wallets.py
@@ -4,17 +4,19 @@ from google.protobuf.json_format import MessageToDict
 
 client = PowerGateClient("127.0.0.1:5002")
 
+wallets = client.wallet.list()
+print("Wallets:")
+print(wallets)
+
+
 print("Creating a new FFS:")
 newFfs = client.ffs.create()
 print(newFfs)
 
-wallet = client.ffs.addrs_list(newFfs.token)
-obj = MessageToDict(wallet)
+address = client.ffs.addrs_list(newFfs.token)
+obj = MessageToDict(address)
+wallet = obj["addrs"][0]["addr"]
+print("FFS wallet:" + wallet)
 
-print(obj['addrs'][0]['addr'])
-
-print("wallet type:")
-print(type(wallet))
-print(type(obj))
-
-
+balance = client.wallet.balance(wallet)
+print("Wallet balance: " + balance)
