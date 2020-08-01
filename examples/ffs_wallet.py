ad4cd0a609d23bde834092d2480ed1df3c6a2857
Leonard Ge <wge@confluent.io>
2020-08-01 11:21:45 +0100

    Fixed examples.

---
+++ examples/ffs_wallet.py
@@ -0,0 +1,22 @@
+from pygate_grpc.client import PowerGateClient
+from google.protobuf.json_format import MessageToDict
+
+
+client = PowerGateClient("127.0.0.1:5002")
+
+wallets = client.wallet.list()
+print("Wallets:")
+print(wallets)
+
+
+print("Creating a new FFS:")
+newFfs = client.ffs.create()
+print(newFfs)
+
+addresses = client.ffs.addrs_list(newFfs.token)
+obj = MessageToDict(addresses)
+wallt = obj["addrs"][0]["addr"]
+print("FFS wallet: " + wallt)
+
+balance = client.wallet.balance(wallt)
+print("Wallet balance: ", str(balance.balance))
