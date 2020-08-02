3d71c49140e9d11ba4d90e07b3ce4f249c830834
apogiatzis <apoyiatzis@outlook.com>
2020-08-02 14:32:31 +0300

    fix: add deprecated to setup

--- setup.py
+++ setup.py
@@ -8,7 +8,13 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    install_requires=["grpcio==1.30.0", "protobuf==3.12.2", "six==1.15.0"],
+    install_requires=[
+        "deprecated==1.2.10",
+        "grpcio==1.30.0",
+        "protobuf==3.12.4",
+        "six==1.15.0",
+        "wrapt==1.12.1",
+    ],
     name="pygate_grpc",
     version="0.0.9",
     description="A Python interface to Textile's Powergate Filecoin API",
