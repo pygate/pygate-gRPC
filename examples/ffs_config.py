1477e4bd4db4939a1951acaa544c62e12e861304
Peter Van Garderen <peter@artefactual.com>
2020-08-03 15:45:14 -0700

    WIP: change default config

--- examples/ffs_config.py
+++ examples/ffs_config.py
@@ -1,3 +1,6 @@
+import json
+from google.protobuf.json_format import Parse
+from google.protobuf.message import Message
 from pygate_grpc.client import PowerGateClient
 
 client = PowerGateClient("127.0.0.1:5002")
@@ -8,4 +11,16 @@ tk = newFfs.token
 print("Token: " + tk)
 print("Using the new FFS token to request the default config:")
 defaultConfig = client.ffs.default_config(tk)
+print(defaultConfig)
+
+print("Loading new default config...")
+with open("cidconfig.json", "r") as f:
+    config = f.read()
+message new_msg {}
+
+new_config = Parse(config, new_msg)
+client.ffs.set_default_config(new_config, tk)
+
+defaultConfig = client.ffs.default_config(tk)
+print("Updated default config:")
 print(defaultConfig)
