5c4b6b8f03b20137ac8411b300b5f09687ef5e70
Leonard Ge <gw980531@gmail.com>
2020-09-16 18:09:07 +0800

    Improved error handling.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -81,7 +81,7 @@ def pytest_unconfigure(config):
     """Runs before test process exits. Cleans up any artifacts from configure"""
     try:
         shutil.rmtree(REPO_LOCAL_PATH)
-    except OSError:
+    except Exception:
         logger.warning("Couldn't delete powergate repository. Maybe it wasn't cloned in the first place")
 
 
