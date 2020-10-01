86c4e353bdca57f3710eb14f3b83aa1d8127f1c7
APogiatzis <39923420+apogiatzis@users.noreply.github.com>
2020-10-01 01:02:09 +0100

    fix: integration tests (#44)

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -24,11 +24,14 @@ def is_docker_running():
     """Checks if docker is running"""
     logger.debug("Checking if docker is running...")
     client = docker.from_env()
+    is_running = True
     try:
         client.info()
-        return True
     except Exception as e:
-        return False
+        is_running = False
+    finally:
+        client.close()
+    return is_running
 
 
 def is_docker_compose_installed():
